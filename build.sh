275fea3a8c615924821ab9c77e86b763e7a8cc5c
murer <murer@dextra-sw.com>
2021-10-01 15:22:47 -0300

    x

--- build.sh
+++ build.sh
@@ -14,6 +14,13 @@ cmd_sa() {
     docker run --name sonar-sa --rm -p 9000:9000 -it murer/sonar-sa:dev entrypoint/sonar-sa.sh "$@"
 }
 
+cmd_server() {
+    docker run --name sonar-sa --rm -it \
+        -p 9000:9000 \
+        murer/sonar-sa:dev \
+        entrypoint/sonar-sa.sh server_only
+}
+
 cmd_main() {
     sonarsa_project="${1?'sonarsa_project'}"
     [[ -d "$sonarsa_project" ]]
@@ -26,6 +33,16 @@ cmd_main() {
 
 cmd_sample_python() {
     cmd_main "$(pwd)/sample/sonar-sa-sample-python"
+}
+
+cmd_sample_local_java() {
+    sonarsa_token="$(docker exec -i sonar-sa cat /tmp/sonarqa.token)"
+    cd sample/sonar-sa-sample-java
+    mvn clean install sonar:sonar \
+        -Dsonar.projectKey=local \
+        -Dsonar.host.url=http://localhost:9000 \
+        "-Dsonar.login=$sonarsa_token"
+    cd -
 }
 
 cmd_shell() {
